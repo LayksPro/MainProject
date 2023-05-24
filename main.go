7a86190d7958fd85e1409a9fc85de98d71a4332f
LayksPro <a.paramonov11@gmail.com>
2023-05-24 17:21:17 +0300

    Новый

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Disscard")
+	var name string
+	var age int
+	fmt.Print("Введите имя: ")
+	fmt.Fscan(os.Stdin, &name)
+
+	fmt.Print("Введите возраст: ")
+	fmt.Fscan(os.Stdin, &age)
+
+	fmt.Println(name, age)
 }
