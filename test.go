984a613a55ce0739c2ecde1a57635e359e814678
LayksPro <a.paramonov11@gmail.com>
2023-07-19 13:11:12 +0300

    Fix

--- test.go
+++ test.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Мой пароль, говно")
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = ""
+	}
+	fmt.Println(s)
 }
